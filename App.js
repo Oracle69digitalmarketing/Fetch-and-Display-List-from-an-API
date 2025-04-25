b68cd0abe16492499b8773929157221754b8be33
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-04-25 19:58:24 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ClickCounterApp from './components/ClickCounterApp';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React Project</h1>
+      <ClickCounterApp />
+    </div>
+  );
+}
+
+export default App;
