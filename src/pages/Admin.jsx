d385e868b531e7b293c72df6df8e0d53991c8a7c
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-04-25 19:38:39 +0100

    Create src/pages/Admin.jsx

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,10 @@
+import Dashboard from './Dashboard';
+
+export default function Admin() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-2">Admin Panel</h1>
+      <Dashboard />
+    </div>
+  );
+}
